217d9dc5e8187b14afa8eaf76a12a3c6c2d442b7
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2024-03-14 15:51:03 +0100

    Starting checkin component

---
+++ src/app/services/checkin/checkin.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckinService {
+
+  private BASE_URL: string = 'http:'
+
+  constructor() { }
+}
