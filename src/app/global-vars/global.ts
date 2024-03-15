5a065a186aaef686974f19b32cc9f08c5692840b
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2024-03-15 21:23:37 +0100

    Global variables update

--- src/app/global-vars/global.ts
+++ src/app/global-vars/global.ts
@@ -1,5 +1,8 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export const Constants = {
-    BASE_URL: "http://192.168.192.108:8080/"
+    BASE_URL: "http://192.168.192.108:8080/",
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
 }
